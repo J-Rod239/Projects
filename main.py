044c3f977596708183520dd899d78fbcc184ddd4
J-Rod239 <65746811+J-Rod239@users.noreply.github.com>
2024-01-03 09:09:20 -0500

    Update main.py
    Added missing code back

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+def check_amount():
+    check_input = input("Enter the check amount: ")
+
+    try:
+        check_total = int(check_input)
+        # Split check into percentages based on the 50/30/20 rule
+        split_50 = check_total * 0.5
+        split_30 = check_total * 0.3
+        split_20 = check_total * 0.2
+        return check_total, split_50, split_30, split_20
+    except ValueError:
+        print("Please enter a valid integer.")
+        return None
+
+def bills():
+    bill_input = input("Enter total of bills for the month: ")
+    try:
+        bill_total = int(bill_input)
+        return bill_total
+    except ValueError:
+        print("Please enter a valid integer.")
+        return None
+
 def input_transactions():
     try:
         num_transactions = int(input("Enter the number of transactions: "))

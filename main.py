2764bb2ac29874514be69b666d68003da2bcc9f8
J-Rod239 <65746811+J-Rod239@users.noreply.github.com>
2024-01-02 23:18:28 -0500

    Update main.py
    Added 50/30/20 rule to program for check breakdown

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ def check_amount():
 
     try:
         check_total = int(check_input)
-        return check_total
+        #Split check into percentages based on the 50/30/20 rule
+        split_50 = check_total * 0.5
+        split_30 = check_total * 0.3
+        split_20 = check_total * 0.2
+        return check_total, split_50, split_30, split_20
     except ValueError:
         print("Please enter a valid integer.")
         return None
@@ -21,7 +25,11 @@ check_result = check_amount()
 bills_result = bills()
 
 if check_result is not None and bills_result is not None:
-    print("Remaining amount:", check_result - bills_result)
+    check_total, split_50, split_30, split_20 = check_result
+    print("Remaining amount after bills:", check_total - bills_result)
+    print("50% Split:", split_50)
+    print("30% Split:", split_30)
+    print("20% Split:", split_20)
 else:
     print("Program crashed")
     

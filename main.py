fc04f0d95e7edf0eca54453452e56b4939c03ab5
J-Rod239 <ProJose239@outlook.com>
2024-01-03 22:11:29 -0500

    changed transactions question to ask user if they want to add transactions

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ def check_amount():
     check_input = input("Enter the check amount: ")
 
     try:
-        check_total = int(check_input)
+        check_total = float(check_input)
         # Split check into percentages based on the 50/30/20 rule
-        split_50 = check_total * 0.5
-        split_30 = check_total * 0.3
-        split_20 = check_total * 0.2
+        split_50 = round(check_total * 0.5, 2)
+        split_30 = round(check_total * 0.3, 2)
+        split_20 = round(check_total * 0.2, 2)
         return check_total, split_50, split_30, split_20
     except ValueError:
         print("Please enter a valid integer.")
@@ -15,23 +15,30 @@ def check_amount():
 def bills():
     bill_input = input("Enter total of bills for the month: ")
     try:
-        bill_total = int(bill_input)
+        bill_total = float(bill_input)
         return bill_total
     except ValueError:
         print("Please enter a valid integer.")
         return None
 
 def input_transactions():
-    try:
-        num_transactions = int(input("Enter the number of transactions: "))
-        transactions = []
-        for i in range(num_transactions):
-            transaction_amount = float(input(f"Enter amount for transaction {i + 1}: "))
-            transactions.append(transaction_amount)
-        return transactions
-    except ValueError:
-        print("Please enter a valid integer for the number of transactions and valid floats for transaction amounts.")
-        return None
+    while True:
+        transaction_q = input("Do you want to add any transactions? (yes/no): ").lower()
+        if transaction_q == 'yes':
+            try:
+                num_transactions = int(input("Enter the number of transactions: "))
+                transactions = []
+                for i in range(num_transactions):
+                    transaction_amount = float(input(f"Enter amount for transaction {i + 1}: "))
+                    transactions.append(transaction_amount)
+                return transactions
+            except ValueError:
+                print("Please enter a valid integer for the number of transactions and valid floats for transaction amounts.")
+                return None
+        elif transaction_q == 'no':
+            return None
+        else:
+            print("Please enter 'yes' or 'no'.")
 
 def credit_card_debt():
     while True:

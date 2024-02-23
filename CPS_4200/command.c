fbe96854ab35ea23f7af7f2fac192ad628868c1d
J-Rod239 <65746811+J-Rod239@users.noreply.github.com>
2024-02-23 14:40:00 +0000

    fixed

--- CPS_4200/command.c
+++ CPS_4200/command.c
@@ -3,7 +3,8 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc != num + 2)
+    int numIntegers = atoi(argv[1]);
+    if (argc != numIntegers + 2)
     {
         printf("Error: You have to enter three integers.\n");
         return 1;

3b222503b3dbf437b4389211d0b1f123b554873e
J-Rod239 <65746811+J-Rod239@users.noreply.github.com>
2024-02-23 14:37:22 +0000

    error check added

--- CPS_4200/command.c
+++ CPS_4200/command.c
@@ -3,6 +3,11 @@
 
 int main(int argc, char *argv[])
 {
+    if (argc != num + 2)
+    {
+        printf("Error: You have to enter three integers.\n");
+        return 1;
+    }
     for (int i = 2; i < argc; i++)
     {
         int num = atoi(argv[i]);

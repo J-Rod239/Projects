813425fbec211f06b670202a6886442744854644
J-Rod239 <ProJose239@outlook.com>
2024-08-19 12:02:17 -0400

    expense app

---
+++ Expense_Tracker/expenses/src/App.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import Tracker from "./components/Tracker";
+import GlobalStyles from "./globalStyles";
+
+const Main = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const App = () => {
+  return (
+    <Main>
+      <GlobalStyles />
+      <Tracker />
+    </Main>
+  )
+}
+
+export default App;
---
+++ Expense_Tracker/expenses/src/OverviewComponent.js
@@ -0,0 +1,44 @@
+
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 25px;
+`;
+
+const Balance = styled.h2`
+  font-weight: 500;
+  & span {
+    font-weight: bold;
+  }
+`;
+
+const AddBtn = styled.button`
+  cursor: pointer;
+  background-color: rgb(68, 230, 16);
+  color: rgb(255, 255, 255);
+  padding: 7px 20px;
+  font-size: 16px;
+  border: none;
+  text-transform: uppercase;
+  border-radius: 5px;
+`;
+
+const OverviewComponent = ({ toggle, setToggle, income, expense }) => {
+  const bal = income - expense;
+
+  return (
+    <Container>
+      <Balance>
+        Balance <span>₹{bal}</span>
+      </Balance>
+      <AddBtn onClick={() => setToggle(!toggle)}>
+        {toggle ? "Cancel" : "Add"}
+      </AddBtn>
+    </Container>
+  );
+};
+
+export default OverviewComponent;
